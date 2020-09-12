294ca8d2df3121e8f816493ce368d0e26ae7f49f
Jinseon Shin <jinseonshin@Jinseons-MacBook-Pro.local>
2020-09-12 19:24:30 -0400

    created actionTypes, actions, LandingPageContainer; added reducers and updated App.jsx

--- client/actions/actions.js
+++ client/actions/actions.js
@@ -1,11 +1,32 @@
 // import actionType constants
 import * as types from '../constants/actionTypes'
 
+
 //export functions that return action objects
   //objects have two props
   //type: a reference to the consts, tells which reducer method to use
   //payload: whatever data the reducer will need
   
+export const retrieveAllRecipes = () => ({
+  type: types.HOME_RECIPES,
+  payload: null
+})
+
+export const retrieveRecipe = (recipeId) => ({
+  type: types.RETRIEVE_RECIPE,
+  payload: recipeId
+})
+
+export const createRecipe = () => ({
+  type: types.CREATE_RECIPE,
+  payload: null
+})
+
+export const inputRecipeData = (data) => ({
+  type: types.INPUT_RECIPE_DATA,
+  payload: data
+})
+
   //ex:
   // export const addCard = (marketId) => ({
   //   type: types.ADD_CARD,
--- client/reducers/recipeReducer.js
+++ client/reducers/recipeReducer.js
@@ -1,7 +1,39 @@
 //import action types
+import * as types from '../constants/actionTypes'
+import { Switch } from 'react-router'
 
 //set initial state
+const initialState = {
+  recipesList: [], //home_recipe: recipes from get request to database
+  newRecipe: {
+    name: '',
+    imageLink: '',
+    ingredients: [],
+    instructions: '',
+    creator: 'WUNDERPUSS',
+  },
+  retrievedRecipe: {},
+}
 
 //declare reducer and its methods
+const recipeReducer = (state = initialState, action) => {
+  
+  switch (action.type){
+    case types.HOME_RECIPES: 
+      const retrieveRecipesFromDB = () => {
+        fetch('/api')
+          .then(res => res.json())
+          .then(data => {
+            return {
+              ...state, 
+              recipesList: data.rows
+            };
+          })
+      }
+    
+    default: 
+      return state; 
+  }
+}
 
-//export reducer
+export default recipeReducer
