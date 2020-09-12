f159cc418cdc74b5c08c340a395c9ca0c9bd3e12
Alexander Kim <akim3235@gmail.com>
2020-09-11 17:14:03 -0700

    Initial setup: Webpack, Express server, html, package.json.

---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+const path = require('path');
+
+module.exports = {
+
+  mode: process.env.NODE_ENV,
+  entry: './client/index.js',
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'bundle.js'
+  },
+
+  devServer: {
+    publicPath: '/build/',
+    contentBase: './client',
+    proxy: {
+      '/': 'http://localhost:3000'
+    }
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.jsx?/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react']
+          }
+        }
+      },
+      {
+        test: /\.css/,
+        use: ['style-loader', 'css-loader'],
+      }
+    ]
+  }
+  
+}
